5342e98622510937eeeadacabd7f7eda294fd167
M. Isuru Tharanga Chrishantha Perera <isuru@apache.org>
2015-07-26 07:50:41 +0530

    Added getters and setters for Doctor entity

--- projects/legacy-system/src/main/java/lk/ac/cmb/ucsc/mcs/ewa/entity/Doctor.java
+++ projects/legacy-system/src/main/java/lk/ac/cmb/ucsc/mcs/ewa/entity/Doctor.java
@@ -1,5 +1,7 @@
 package lk.ac.cmb.ucsc.mcs.ewa.entity;
 
+import java.io.Serializable;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -8,13 +10,17 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
 @XmlRootElement(name = "Doctor")
-public class Doctor {
+public class Doctor implements Serializable {
+
+    private static final long serialVersionUID = -4349052212589556727L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
     private String firstName;
     private String lastName;
+    private String username;
+    private String password;
 
     public Doctor() {
     }
@@ -22,6 +28,71 @@ public class Doctor {
     public Doctor(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((getUsername() == null) ? 0 : getUsername().hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (!(obj instanceof Doctor))
+            return false;
+        Doctor other = (Doctor) obj;
+        if (getUsername() == null) {
+            if (other.getUsername() != null)
+                return false;
+        } else if (!getUsername().equals(other.getUsername()))
+            return false;
+        return true;
     }
 
     @Override
